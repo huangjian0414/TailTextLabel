fabfff160d25c7fd99dbafcf59ce94090461bc05
huangjian <huangjian@lanyife.com>
2019-10-18 11:40:53 +0800

    Initial Commit

---
+++ TruncationText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TruncationText
+//
+//  Created by huangjian on 2019/10/18.
+//  Copyright © 2019 huangjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
